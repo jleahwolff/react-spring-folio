d2dc17f3f2d3adc3130b910a45233accf644b606
Jessica Wolff <jleahwolff@gmail.com>
2020-04-01 01:57:31 -0500

    Effects, Desc, Flavors built. Done for the night.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './sass/output.css';
 import ParallaxComponent from "./components/ParallaxComponent.js";
 import Navigation from "./components/Navigation";
 import ReactTooltip from 'react-tooltip';
+import letters from "./assets/letters.png";
 
 function App() {
 
@@ -12,7 +13,10 @@ function App() {
       <ParallaxComponent />
       <ReactTooltip />
       <div className="footer">
+        <div className="footer-logo">
+        <img className="footerimg" src={letters} />
         <h4> Strain Watch ® </h4>
+        </div>
         <p>Careers | Money | College | Loans | Coronavirus | Stuck-at-home | Ozarks on Netflix</p>
       </div>
     </div>
